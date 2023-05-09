093e83011a1f9191b75ee4e23f778d74d273e170
Stadnik Andrii <stadnikandriy1@gmail.com>
2023-05-09 10:29:20 +0300

    Add reprojectpoints function to reproject.py

--- calibration/solver/solve.py
+++ calibration/solver/solve.py
@@ -7,14 +7,27 @@ from .intrinsics import solve_intrinsic
 def solve(
     x: np.ndarray, X: np.ndarray, intrinsic_matrix: np.ndarray
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    # x = np.c_[x, np.ones(x.shape[0])]
-    # x = np.linalg.pinv(intrinsic_matrix) @ x.T
-    # x = x.T[:, :2]
-    x -= intrinsic_matrix[:2, 2]
-    assert np.linalg.norm(x, axis=0).shape == (2,)
+    """Find intrinsic and extrinsic parameters for a set of points
+
+    Args:
+        x: [x, y] mx2 array of image points
+        X: [x, y] mx2 array of board points
+        intrinsic_matrix: Intrinsic matrix
+
+    Returns:
+        lambdas: [2x1] array of camera parameters
+        R: [3x3] rotation matrix
+        t: [1x1] translation
+    """
+    x = np.c_[x, np.ones(x.shape[0])]
+    x = np.linalg.pinv(intrinsic_matrix) @ x.T
+    x = x.T[:, :2]
+
+    # x -= intrinsic_matrix[:2, 2]
+    # assert np.linalg.norm(x, axis=0).shape == (2,)
     # biggest_r_i = np.argmax(np.linalg.norm(x, axis=1))
-    biggest_r = np.max(np.linalg.norm(x, axis=1))
-    x /= biggest_r
+    # biggest_r = np.max(np.linalg.norm(x, axis=1))
+    # x /= biggest_r
     # x_max = x.max(axis=0)
     # print(x_max.shape)
     # x /= x_max
@@ -22,6 +35,6 @@ def solve(
     lambdas, t_3 = solve_intrinsic(x, X, p)
     p[2, 2] = t_3
     t = p[:, 2]
-    t *= biggest_r
+    # t *= biggest_r
     R = np.c_[p[:, :2], np.cross(p[:, 0], p[:, 1])]
     return lambdas, R, t
--- main.py
+++ main.py
@@ -1,50 +1,53 @@
 import numpy as np
-import plotly.express as px
-import pandas as pd
 from icecream import ic
 
-from calibration.benchmark.benchmark import gen_data
-from calibration.simulator.board import gen_checkerboard_grid
-from calibration.simulator.simul import SimulParams, simul_projection
+from calibration.simulator.board import draw_board, gen_checkerboard_grid
+from calibration.simulator.simulator import Projector
+from calibration.solver.reproject import reprojectpoints
 from calibration.solver.solve import solve
-from calibration.data.babelcalib.orpc import load_babelcalib
 
 
 def test_solver():
-    # params = SimulParams(
+    # projector = Projector(
     #     R=np.eye(3), t=np.array([-4, -3, 15]), lambdas=np.array([0.0, 0.0])
     # )
-    params = SimulParams(R=np.eye(3))
-    # params.t[:2] = params.camera.principal_point
-    # params = SimulParams()
+    projector = Projector(R=np.eye(3))
+    # projector.t[:2] = projector.camera.principal_point
+    # projector = Projector()
 
-    # X, x, lambdas, R, t = simul_projection(gen_charuco_grid(7, 9, 0.4, 0.2), params)
-    X, x, lambdas, R, t = simul_projection(gen_checkerboard_grid(7, 9), params)
+    # X, x, lambdas, R, t = simul_projection(gen_charuco_grid(7, 9, 0.4, 0.2), projector)
+    X = gen_checkerboard_grid(7, 9)
+    x = projector.project(X)
+    # X, x, lambdas, R_inv, t_inv =
 
     # print(x)
-    # draw_board(x, max_xy=params.camera.resolution).show()
+    # draw_board(x, max_xy=projector.camera.resolution).show()
+    draw_board(X).show()
+    draw_board(projector.backproject(X)).show()
 
     assert (x > 0).all()
-    assert (x < params.camera.resolution).all()
-    lambdas_, R_, t_ = solve(x, X, params.camera.intrinsic_matrix)
-    ic(R.round(3))
-    ic(R_.round(3))
-    ic(t.round(3))
-    ic(t_.round(3))
-    ic(lambdas.round(3), lambdas_.round(3))
+    assert (x < projector.camera.resolution).all()
+    lambdas_, R_inv_, t_inv_ = solve(x, X, projector.camera.intrinsic_matrix)
+    ic(projector.R.round(3))
+    ic(R_inv_.round(3))
+    ic(projector.t.round(3))
+    ic(t_inv_.round(3))
+    ic(projector.lambdas.round(3), lambdas_.round(3))
+
+    reprojectpoints(x, X, projector.lambdas, projector.R, projector.t)
 
 
 if __name__ == "__main__":
     # np.random.seed(44)
-    # test_solver()
+    test_solver()
     # df = gen_data()
     # df.to_pickle("/tmp/data.pkl")
     # df = pd.read_pickle("/tmp/data.pkl")
-    datasets = load_babelcalib()
-    for ds in datasets:
-        pass
-        # print(ds.name)
-        # print(len(ds.test))
-        # print(len(ds.train))
-        # px.imshow(ds.test[0].image).show()
-        # break
+    # datasets = load_babelcalib()
+    # for ds in datasets:
+    #     pass
+    # print(ds.name)
+    # print(len(ds.test))
+    # print(len(ds.train))
+    # px.imshow(ds.test[0].image).show()
+    # break
