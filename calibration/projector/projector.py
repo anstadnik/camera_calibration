61922bd57bdca783337016b04a0456899414d3ad
Andrii Stadnik <stadnikandriy1@gmail.com>
2023-05-13 16:54:40 +0300

    Update unittest.yml and projector.py files

--- calibration/projector/projector.py
+++ calibration/projector/projector.py
@@ -8,7 +8,7 @@ from .camera import Camera
 
 
 def _gen_lambdas() -> np.ndarray:
-    l1 = np.random.uniform(-5, 5)
+    l1 = np.random.uniform(-1.5, 1.5)
     l2 = np.random.uniform(
         -2.61752136752137 * l1 - 6.85141810943093,
         -2.61752136752137 * l1 - 4.39190876941320,
@@ -45,7 +45,13 @@ class Projector:
 
     R: np.ndarray = field(default_factory=lambda: qr(np.random.randn(3, 3))[0])
     t: np.ndarray = field(
-        default_factory=lambda: np.random.uniform([6.0, 4.0, -20.0], [2.0, 2.0, -100.0])
+        # default_factory=lambda: np.random.uniform([2.0, 2.0, -100.0], [6.0, 4.0, -20.0])
+        default_factory=lambda: np.random.uniform(
+            # [-6.0, -4.0, 20.0], [-2.0, -2.0, 100.0]
+            # [-6.0, -4.0, 2.0], [-2.0, -2.0, 10.0]
+            [-0.3, -0.3, 1.5],
+            [0.3, 0.3, 3.0],
+        )
     )
     lambdas: np.ndarray = field(default_factory=_gen_lambdas)
     camera: Camera = field(default_factory=Camera)
@@ -68,40 +74,52 @@ class Projector:
         # Extrinsics
         X_h = np.c_[X, np.ones(X.shape[0])]
         P = np.c_[self.R[:, :2], self.t]
-        P_inv = np.linalg.inv(P)
-        x = (P_inv @ X_h.T).T
+        x = (P @ X_h.T).T
         x /= x[:, 2][:, None]
-        np.testing.assert_almost_equal(x[:, 2], 1)
+        x.copy()
 
         # Distortion
+        # distortion_center = (
+        #     np.linalg.inv(self.camera.intrinsic_matrix)
+        #     @ np.r_[self.camera.resolution, 1]
+        # ) / 2
+        # x[:, :2] -= distortion_center[:2]
+
         idx = np.linalg.norm(x[:, :2], axis=1) > 0
 
-        # X = [lu, lv, lpsi(|[u, v]|)]
-        # X = [lu/lpsi(|[u, v]|), lv/lpsi(|[u, v, 1]|), 1]
-        # X = [u/psi(|[u, v]|), v/psi(|[u, v, 1]|), 1]
-
-        # psi(r) / r == x[2] / norm(x[:2]) => psi(r) * norm(x:[2]) - r * x[2] == 0
-        def f(r, x):
+        def f(lbd, x):
             # x[2] == 1
-            return np.linalg.norm(x[:2]) * self.psi(r) - r
+            return self.psi(np.linalg.norm(x[:2]) / lbd) - x[2] / lbd
 
-        max_r = np.linalg.norm(self.camera.resolution) / 2
-        r_hat = np.array(
-            [root_scalar(f, args=(xi,), bracket=(0, max_r)).root for xi in x[idx]]
+        max_lambda = np.linalg.norm(self.camera.resolution) / 2
+        lambdas = np.array(
+            [
+                root_scalar(
+                    f, args=(xi,), bracket=(0 + np.finfo(float).eps, max_lambda)
+                ).root
+                for xi in x[idx]
+            ]
         )
 
-        # x[idx] *= (self.psi(r_hat) / x[idx, 2])[:, np.newaxis]
-        x[idx] *= (r_hat / np.linalg.norm(x[:, :2], axis=1))[:, np.newaxis]
-        np.testing.assert_almost_equal(
-            self.psi(np.linalg.norm(x[:, :2], axis=1)), x[:, 2]
-        )
+        assert np.linalg.norm(x[:, :2], axis=1).shape == (63,)
+        assert lambdas.shape == (63,)
+        x[idx] /= lambdas[:, np.newaxis]
+        # np.testing.assert_array_almost_equal(x[:, 1] / x[:, 0], x1[:, 1] / x1[:, 0])
+        # print(np.c_[x[:, 1] / x[:, 0], x1[:, 1] / x1[:, 0]][:20])
+        # np.testing.assert_almost_equal(
+        #     self.psi(np.linalg.norm(x[:, :2], axis=1)), x[:, 2], decimal=5
+        # )
         x[:, 2] = 1
+        # x[:, :2] += distortion_center[:2]
+        x.copy()
+        # np.testing.assert_array_almost_equal(x2[:, 1] / x2[:, 0], x1[:, 1] / x1[:, 0])
 
         # Intrinsics
         x = (self.camera.intrinsic_matrix @ x.T).T
         # Pyright bug
         x /= x[:, 2][:, None]  # type: ignore
         return x[:, :2]
+        # return x1[:, :2], x2[:, :2], x[:, :2]
 
     def backproject(self, x: np.ndarray) -> np.ndarray:
         """
@@ -130,7 +148,8 @@ class Projector:
 
         # Extrinsics
         P = np.c_[self.R[:, :2], self.t]
-        x = (P @ x.T).T
+        P_inv = np.linalg.inv(P)
+        x = (P_inv @ x.T).T
         x /= x[:, 2][:, None]
         return x[:, :2]
 
