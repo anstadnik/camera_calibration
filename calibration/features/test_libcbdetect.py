3348bff6fe3509fe457065d5dbf3820cdd61026f
Andrii Stadnik <stadnikandriy1@gmail.com>
2023-05-15 19:47:35 +0300

    Refactor features module

--- calibration/features/test_libcbdetect.py
+++ calibration/features/test_libcbdetect.py
@@ -1,16 +1,39 @@
+# pyright: reportUnusedImport=false
 import unittest
 
+import numpy as np
+from PIL import Image
 
-class TestImports(unittest.TestCase):
-    def test_imports(self):
+
+class TestFeatures(unittest.TestCase):
+    def test_feature_detection(self):
         try:
-            from cbdetect_py import (  # noqa: unused-imports
+            from cbdetect_py import (
                 CornerType,
                 Params,
                 boards_from_corners,
                 find_corners,
             )
-
-            self.assertTrue(True)
         except ImportError:
             self.fail("ImportError: One or more imports failed")
+
+        img_path = "./data/BabelCalib/UZH/DAVIS/outdoor_forward/train/img1188.pgm"
+        with Image.open(img_path) as img:
+            img.load()
+        img = np.array(img)
+        params = Params()
+        params.show_processing = False
+        params.corner_type = (
+            CornerType.SaddlePoint
+            # if target.type == BoardType.RECTANGULAR
+            # else CornerType.MonkeySaddlePoint
+        )
+
+        # img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+
+        corners = find_corners(img, params)
+        # plot_corners(img, corners)
+        boards = boards_from_corners(img, corners, params)
+
+        assert corners.p
+        assert boards
